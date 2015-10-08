8c01fbb26bedc1399fb104cb6a83fa0ffe856a66
subercui <subercui@sina.com>
2015-10-08 18:06:42 +0800

    add WiderRNN/

--- auto_updateLighting.py
+++ auto_updateLighting.py
@@ -14,6 +14,7 @@ if not os.path.exists('/data/pm25data/dataset/RNNPm25Dataset'+today.strftime('%Y
     os.system('scp suber@swarma.net:/ldata/pm25data/pm25dataset/RNNPm25Dataset'+today.strftime('%Y%m%d')+'_t100p100shuffled.pkl.gz /data/pm25data/dataset/')
 if not os.path.exists('/data/pm25data/dataset/RNNPm25Dataset'+today.strftime('%Y%m%d')+'_t100p100shuffled.pkl.gz'):
     os.system('echo "Pm25 RNN Dataset file generating error!" | mail -s "caiyun pm25 alarm from lighting" "subercui@sina.com"')
+    sys.exit(-1)
 print "dataset downloaded"
 
 #update model
@@ -21,6 +22,7 @@ os.system('python /home/suber/Git/RNN_pm25/model/Pm25RNN_MINIBATCH.py')
 print "RNN model trained"
 if not os.path.exists('/data/pm25data/model/RNNModel'+today.strftime('%Y%m%d')+'.pkl.gz'):
     os.system('echo "Pm25 RNN model generating error!" | mail -s "caiyun pm25 alarm from lighting" "subercui@sina.com"')
+    sys.exit(-1)
 
 #scp
 os.system('scp /data/pm25data/model/RNNModel'+today.strftime('%Y%m%d')+'.pkl.gz caiyun@api.dev2.caiyunapp.com:/ldata/pm25data/pm25model/')
